53041e85dd206dd5b004ac97381042fbc2db91c5
Alex Goncharov <misterksn@gmail.com>
2024-06-02 11:52:12 +0300

    Refactor JWT-none task

--- jwt-none/task/app/app.py
+++ jwt-none/task/app/app.py
@@ -1,13 +1,11 @@
-from flask import Flask, request, render_template, session, redirect, url_for
-import jwt
-import time
-import jwt
 import secrets
 import string
+import time
 
+import jwt
+from flask import Flask, make_response, redirect, render_template, request, url_for
 
 app = Flask(__name__, template_folder='templates')
-
 app.secret_key = 'super secret key'
 
 real_flag = ''
@@ -30,8 +28,8 @@ def generate_jwt_token(user_name: str):
         "user_name": user_name,
         "exp": int(time.time()) + 3600
     }
-    # token = jwt.encode(payload, super_random_string, algorithm='HS256')
-    token = jwt.encode(payload, None, algorithm=None)
+    token = jwt.encode(payload, super_random_string, algorithm='HS256')
+    # token = jwt.encode(payload, None, algorithm=None)
     return token
 
 
@@ -40,56 +38,41 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        
-        if username == 'admin' and password == 'password':
-            token = generate_jwt_token(username)
-            session['token'] = token
-            return redirect(url_for('profile'))
-        else:
-            return "Invalid credentials", 401
-    return render_template('login.html')
-
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        
+
         if username == 'admin':
             return "Only for Admin!", 403
-    
+
         token = generate_jwt_token(username)
-        session['token'] = token
-        return redirect(url_for('profile'))
+
+        response = make_response(redirect(url_for('profile')))
+        response.set_cookie('token', token)
+        return response
+
     return render_template('register.html')
 
 
 @app.route('/profile')
 def profile():
-    options = {
-        "verify_signature": '',
-        "algorithms": ['HS256'],
-        "keys": ['aadad', 'awdawd']
-    }
-    
-    if 'token' not in session:
+    token = request.cookies.get('token')
+    if not token:
         return "Access denied", 402
+
     try:
-        decoded = jwt.decode(session['token'], options=options)
-        user_name = decoded['user_name']
-        
-        if user_name == 'admin':
-            return render_template('admin.html')
-        else:
-            return f"Welcome, User {user_name}!", 200
+        header = jwt.get_unverified_header(token)
+        decoded = jwt.decode(token, super_random_string, algorithms='HS256', options={'verify_signature': header['alg'] != 'none'})
     except jwt.InvalidTokenError:
         return "Invalid token", 403
+
+    user_name = decoded['user_name']
+    if user_name == 'admin':
+        return render_template('admin.html', {'flag': real_flag})
+
+    return f"Welcome, User {user_name}!", 200
 
 
 @app.route('/submit_flag/<flag>', methods=['GET'])
